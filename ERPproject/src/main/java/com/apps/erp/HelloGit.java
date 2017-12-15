7e7b8d42915adeb50aa1b5ef07db27f2dfa1002b
Baese <morendo1213@gmail.com>
2017-12-15 16:15:35 +0900

    Update HelloGit.java

--- ERPproject/src/main/java/com/apps/erp/HelloGit.java
+++ ERPproject/src/main/java/com/apps/erp/HelloGit.java
@@ -12,8 +12,8 @@ public class HelloGit {
 		System.out.println("Hello Git 영섭 :)");
 		System.out.println("Hello Git 영섭");
 
-		System.out.println("");
-		System.out.println("Hello Git 영섭444");
+		System.out.println("충돌");
+		System.out.println(" 충돌 Hello Git 영섭444");
 
 	}
 
